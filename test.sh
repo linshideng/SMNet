dfd8a63a0dbc52048ba04c3d24a10ef7a95207b2
linshideng <48313054+linshideng@users.noreply.github.com>
2021-10-21 22:12:38 +0800

    python

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-ptyhon test.py  --test_folder  ./datasets/LOL/test/low  --output  ./output_test  --modelfile ./model_LOL.pth --modeltype LOL
+python test.py  --test_folder  ./datasets/LOL/test/low  --output  ./output_test  --modelfile ./model_LOL.pth --modeltype LOL
